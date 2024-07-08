989c956f383c7caf173167c8c8d0fdf9e2bb4226
DuongHN2K <nhatduonghoang59@gmail.com>
2024-07-08 13:39:20 +0700

    Added password column to users and volunteer_details table

--- migration/000001_init.sql
+++ migration/000001_init.sql
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS 'departments' (
 CREATE TABLE IF NOT EXISTS 'countries' (
     `id` INT PRIMARY KEY AUTOINCREMENT,
     `name` VARCHAR(45) NOT NULL,
+    `status` TINYINT NOT NULL,
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
@@ -29,9 +30,9 @@ CREATE TABLE IF NOT EXISTS 'users' (
     `role_id` INT NOT NULL,
     `department_id` INT DEFAULT NULL,
     `email` VARCHAR(45) NOT NULL,
+    `password` TEXT NOT NULL,
     `name` VARCHAR(45) NOT NULL,
     `surname` VARCHAR(45) NOT NULL,
-    `volunteer_id` INT DEFAULT NULL,
     `gender` VARCHAR(20) NOT NULL,
     `dob` DATE NOT NULL,
     `mobile` VARCHAR(15) NOT NULL,
@@ -47,9 +48,21 @@ CREATE TABLE IF NOT EXISTS 'users' (
     KEY `fk_users_countries_idx` (`country_id`),
     KEY `fk_users_resident_countries_idx` (`resident_country_id`),
     CONSTRAINT `fk_users_roles` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
-    CONSTRAINT `fk_users_depts` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`),
     CONSTRAINT `fk_users_countries` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`),
     CONSTRAINT `fk_users_resident_countries` FOREIGN KEY (`resident_country_id`) REFERENCES `countries` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS 'volunteer_details' (
+    `id` INT PRIMARY KEY AUTOINCREMENT,
+    `user_id` INT NOT NULL,
+    `department_id` INT NOT NULL,
+    `status` TINYINT NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    KEY `fk_volunteer_details_depts_idx` (`department_id`),
+    KEY `fk_volunteer_details_users_idx` (`user_id`),
+    CONSTRAINT `fk_volunteer_details_depts` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`),
+    CONSTRAINT `fk_volunteer_details_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
 );
 
 CREATE TABLE IF NOT EXISTS 'requests' (
@@ -61,10 +74,10 @@ CREATE TABLE IF NOT EXISTS 'requests' (
     `verifier_id` INT DEFAULT NULL,
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME ON UPDATE CURRENT_TIMESTAMP,
-    KEY `fk_request_user_idx` (`user_id`),
-    KEY `fk_request_verifier_idx` (`verifier_id`),
-    CONSTRAINT `fk_request_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-    CONSTRAINT `fk_request_verifier` FOREIGN KEY (`verifier_id`) REFERENCES `users` (`id`)
+    KEY `fk_requests_users_idx` (`user_id`),
+    KEY `fk_requests_verifiers_idx` (`verifier_id`),
+    CONSTRAINT `fk_requests_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    CONSTRAINT `fk_requests_verifiers` FOREIGN KEY (`verifier_id`) REFERENCES `users` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS 'user_identities' (
@@ -77,6 +90,6 @@ CREATE TABLE IF NOT EXISTS 'user_identities' (
     `place_issued` VARCHAR(100) NOT NULL,
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME ON UPDATE CURRENT_TIMESTAMP,
-    KEY `fk_ui_users_idx` (`user_id`),
-    CONSTRAINT `fk_ui_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+    KEY `fk_user_identities_users_idx` (`user_id`),
+    CONSTRAINT `fk_user_identities_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
