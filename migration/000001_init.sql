113c08f50f373152cb77ba055358fc9e5a07dbaa
DuongHN2K <nhatduonghoang59@gmail.com>
2024-07-08 09:13:56 +0700

    Added created_at and updated_at column for all tables

--- migration/000001_init.sql
+++ migration/000001_init.sql
@@ -3,7 +3,9 @@ USE `volunteer_db`,
 
 CREATE TABLE IF NOT EXISTS 'roles' (
     `id` INT PRIMARY KEY AUTOINCREMENT,
-    `name` VARCHAR(30) NOT NULL
+    `name` VARCHAR(30) NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP NULL
 );
 
 CREATE TABLE IF NOT EXISTS 'departments' (
@@ -11,13 +13,15 @@ CREATE TABLE IF NOT EXISTS 'departments' (
     `name` VARCHAR(45) NOT NULL,
     `address` VARCHAR(100) NOT NULL,
     `status` TINYINT NOT NULL COMMENT '0: inactive\n1: active',
-    `created_at` DATETIME DEFAULT NOT NULL,
-    `updated_at` DATETIME DEFAULT NULL
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP NULL
 );
 
 CREATE TABLE IF NOT EXISTS 'countries' (
     `id` INT PRIMARY KEY AUTOINCREMENT,
-    `name` VARCHAR(45) NOT NULL
+    `name` VARCHAR(45) NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP NULL
 );
 
 CREATE TABLE IF NOT EXISTS 'users' (
@@ -36,8 +40,8 @@ CREATE TABLE IF NOT EXISTS 'users' (
     `avatar` VARCHAR(100) DEFAULT NULL,
     `verification_status` TINYINT DEFAULT 0 COMMENT '0: unverified\n1: verified',
     `status` TINYINT NOT NULL COMMENT '0: inactive\n1: active',
-    `created_at` DATETIME DEFAULT NOT NULL,
-    `updated_at` DATETIME DEFAULT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP NULL,
     KEY `fk_users_roles_idx` (`role_id`),
     KEY `fk_users_depts_idx` (`department_id`),
     KEY `fk_users_countries_idx` (`country_id`),
@@ -55,8 +59,8 @@ CREATE TABLE IF NOT EXISTS 'requests' (
     `status` TINYINT NOT NULL,
     `reject_notes` VARCHAR(255) NOT NULL,
     `verifier_id` INT DEFAULT NULL,
-    `created_at` DATETIME DEFAULT NOT NULL,
-    `updated_at` DATETIME DEFAULT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP NULL,
     KEY `fk_request_user_idx` (`user_id`),
     KEY `fk_request_verifier_idx` (`verifier_id`),
     CONSTRAINT `fk_request_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
@@ -71,6 +75,8 @@ CREATE TABLE IF NOT EXISTS 'user_identities' (
     `status` TINYINT NOT NULL,
     `expiry_date` DATE NOT NULL,
     `place_issued` VARCHAR(100) NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP NULL,
     KEY `fk_ui_users_idx` (`user_id`),
     CONSTRAINT `fk_ui_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
